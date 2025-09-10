b513fe59c608798fd17663b759323adb4292c7df
ganesh14u <paladugusaiganesh@gmail.com>
2025-09-10 16:42:41 +0530

    New Update

--- client/src/Pages/AdminItPrograms.jsx
+++ client/src/Pages/AdminItPrograms.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./AdminItPrograms.css"; // ✅ we'll style it here
+import "./AdminItPrograms.css";
 
 const API_URL = "http://localhost:5000/api/it-programs";
 
